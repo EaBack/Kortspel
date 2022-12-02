3bfd6127f151e42c9bd1cb056cd22b57a42776cb
EaBack <113344913+EaBack@users.noreply.github.com>
2022-12-02 08:39:01 +0100

    add latest commit

---
+++ app/src/main/java/com/example/korrtspel/CardDeck.kt
@@ -0,0 +1,4 @@
+package com.example.korrtspel
+
+class CardDeck {
+}
---
+++ app/src/main/java/com/example/korrtspel/HiLoCardGame.kt
@@ -0,0 +1,11 @@
+package com.example.korrtspel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HiLoCardGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hi_lo_card_game)
+    }
+}
